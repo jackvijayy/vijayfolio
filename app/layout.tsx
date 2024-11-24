e62ee48193ca8f24a67754942de0f0653ab10df3
jackvijayy <jackvijay1108@gmail.com>
2024-11-24 16:26:06 +0530

    og-graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,23 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Vijay",
   description: "This is my portfolio",
+  openGraph: {
+    title: "VijayFolio",
+    description:"This is my portfolio That shows My skills and Experience",
+    url: "https://vijayfolio.vercel.app/",
+    siteName: "vijayfolio",
+    images: [
+      {
+        url: "https://images.app.goo.gl/7aWWdvFYKjU8KgZM7",
+        width: 1200,
+        height: 630,
+        alt: "A description of the image",
+      },
+    ],
+    locale: "en_US",
+    type: "website", 
+  },
+ 
 };
 
 export default function RootLayout({
