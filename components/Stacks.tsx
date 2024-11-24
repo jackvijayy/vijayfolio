7c7871824502b7d5424647d20d44fab13ff17b0d
jackvijayy <jackvijay1108@gmail.com>
2024-11-24 16:19:03 +0530

    vercel config

--- components/Stacks.tsx
+++ components/Stacks.tsx
@@ -8,7 +8,7 @@ import { stacks, tools } from "@/Constants"
 
 const Stacks = () => {
   return (
-    <div className='h-[80vh] bg-black '>
+    <div className='h-[30vh] bg-black '>
          <div className="w-full ">
         <Marquee pauseOnHover>
           <div className="flex items-center gap-8">
